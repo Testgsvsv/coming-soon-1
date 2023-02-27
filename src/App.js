95ff5e2f43c19b1d799bdd7f7911de1caa026946
Yash Devkate <78010388+rootxyash@users.noreply.github.com>
2023-02-27 18:14:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">TEKNODEON 2K23</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            <a href="https://instagram.com/teknodeon2k23" title="Instagram" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+            <a href="https://twitter.com/teknodeon" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/teknodeon" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
             </a>
           </div>
@@ -27,14 +27,14 @@ class App extends React.Component {
         <div className="content">
           <div className="title-holder">
             <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious to share ideas.</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:yash@teknodeon2k23.tech">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          <span>Made by <a className="underlined" href="https://yashdevkate.me/" target="_blank" rel="noopener noreferrer">Yash Devkate</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
